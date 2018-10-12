ccb045ead55fd7cb4f0599fc9b6ad5418b18ed37
Pablo Tabares Hernandez <pablother3@gmail.com>
2018-10-12 09:54:23 -0500

    first tests with refactor less repetitive

---
+++ click-counter/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display"> The counter is</h1>
+        <button data-test="increment-button">Increment</button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ click-counter/src/App.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter()});
+
+const setup = (props={}, state=null) => {
+  return shallow(<App {...props}/>)
+}
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`)
+}
+
+test('renders without crashing', () => {
+  const wrapper = setup()
+  const appComponent = findByTestAttr(wrapper, "component-app")
+  expect(appComponent.length).toBe(1)
+});
+
+test('renders increment button', () => {
+  const wrapper = setup()
+  const button = findByTestAttr(wrapper, "increment-button")
+  expect(button.length).toBe(1)
+})
+
+test('render counter display', () => {
+  const wrapper = setup()
+  const counterDisplay = findByTestAttr(wrapper, "counter-display")
+  expect(counterDisplay.length).toBe(1)
+})
+
+test('should counter starts at 0', () => {
+  
+})
+
+test('clicking button increments counter display', () => {
+  
+})
+
+
+
+
