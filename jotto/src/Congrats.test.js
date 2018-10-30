ef8372d7e46c3e902b6085904b2b5bb0b2f41671
Pablo Tabares Hernandez <pablother3@gmail.com>
2018-10-30 15:52:51 -0500

    extract confieg enzyme

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from "enzyme-adapter-react-16";
+import { shallow } from 'enzyme';
+
 
 import {findByTestAttr, checkProps} from '../test/testUtils';
 import Congrats from './Congrats';
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 // Cuidado con tener siempre estas default props tambien en el componente Yo recomiendo mas bien usar default props 
 // desde el componente
