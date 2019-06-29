63d9a2fd1353c47b0b5900c4ce0fb6ade0594b00
Pablo Tabares Hernandez <pablother3@gmail.com>
2019-06-29 18:25:58 -0500

    setup guessed word test

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -1,9 +1,10 @@
 export const actionTypes = {
-  CORRECT_GUESS: 'CORRECT_GUESS'
+  CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD',
 }
+export const guessWord = (guessedWord) => {
+  return function(dispatch, getState) {
 
-export function correctGuess() {
-  return {
-    type: actionTypes.CORRECT_GUESS
-  }
-}
+  };
+};
+
---
+++ jotto/src/integration.test.js
@@ -0,0 +1,29 @@
+import {storeFactory} from '../test/testUtils';
+import {correctGuess} from './actions';
+
+describe('guessed word acion dispatcher', () => {
+  describe('no guessed words', () => {
+    test('should update state correctly for unsuccessful guess', () => {
+      
+    })
+
+    test('should updates state correclty for successful guess', () => {
+      
+    })
+    
+    
+  })
+
+  describe('some guessed words', () => {
+    test('should update state correctly for unsuccessful guess', () => {
+      
+    })
+
+    test('should updates state correclty for successful guess', () => {
+      
+    })
+    
+    
+  })
+  
+})
