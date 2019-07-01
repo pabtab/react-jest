4d25a6ce62b52e0132a3c2467146f653b314450e
Pablo Tabares Hernandez <pablother3@gmail.com>
2019-07-01 17:05:38 -0500

    test axios and moxios

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -1,8 +1,13 @@
+
+import axios from 'axios';
+
 import {getLetterMatchCount} from '../helpers'
+
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD'
 }
 export const guessWord = (guessedWord) => {
   return function(dispatch, getState) {
@@ -20,3 +25,14 @@ export const guessWord = (guessedWord) => {
   };
 };
 
+export const getSecretWord = () => {
+  return (dispatch) => {
+    return axios.get('http://localhost:3030')
+      .then(response => {
+        dispatch({
+          type: actionTypes.SET_SECRET_WORD,
+          payload: response.data
+        })
+      })
+  }
+}
