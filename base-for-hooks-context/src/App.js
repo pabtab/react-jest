431b2b3a42b9c62860c7f17aa27e16b4b64ab254
Pablo Tabares H <pablother3@gmail.com>
2020-06-12 12:33:22 -0500

    jotto to hooks

---
+++ base-for-hooks-context/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div data-test="component-app">
+    </div>
+  );
+}
+
+export default App;
---
+++ base-for-hooks-context/src/App.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { shallow } from "enzyme";
+import {findByTestAttr} from '../test/testUtils'
+
+import App from "./App";
+
+const setup = () => {
+  return shallow(<App />)
+}
+
+test('should render without error', () => {
+  const wrapper = setup()
+  const component = findByTestAttr(wrapper, 'component-app')
+
+  expect(component.length).toBe(1)
+  
+})
+
