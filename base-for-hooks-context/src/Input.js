b226f518f63a9ee80c192a647b356d6a1ab8a4ae
Pablo Tabares H <pablother3@gmail.com>
2020-06-22 17:29:39 -0500

    input submit button

--- base-for-hooks-context/src/Input.js
+++ base-for-hooks-context/src/Input.js
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 const Input = ({ secretWord }) => {
   // Ojo depronto no funciona y toca React.useState
-  const [currentGuess, setCurrentGuess] = React.useState('')
-  
+  const [currentGuess, setCurrentGuess] = React.useState("");
+
   return (
     <div data-test="component-input">
       <form className="form-inline">
@@ -13,12 +13,15 @@ const Input = ({ secretWord }) => {
           type="text"
           placeholder="enter guess"
           value={currentGuess}
-          onChange={evt => setCurrentGuess(evt.target.value)}
+          onChange={(evt) => setCurrentGuess(evt.target.value)}
         />
-        <button 
+        <button
           data-test="submit-button"
           className="btn btn-primary mb-2"
-          onClick={evt => evt.preventDefault()}
+          onClick={(evt) => {
+            setCurrentGuess("");
+            evt.preventDefault()
+          }}
         >
           Submit
         </button>
--- base-for-hooks-context/src/Input.test.js
+++ base-for-hooks-context/src/Input.test.js
@@ -1,40 +1,48 @@
 import React from "react";
 import { shallow } from "enzyme";
-import {findByTestAttr, checkProps} from '../test/testUtils'
+import { findByTestAttr, checkProps } from "../test/testUtils";
 
 import Input from "./Input";
 
-const setup = (secretWord="test") => {
-  return shallow(<Input secretWord={secretWord} />)
-}
+const setup = (secretWord = "test") => {
+  return shallow(<Input secretWord={secretWord} />);
+};
 
-test('should render without error', () => {
-  const wrapper = setup()
-  const component = findByTestAttr(wrapper, 'component-input')
+test("should render without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-input");
 
-  expect(component.length).toBe(1)
-  
-})
+  expect(component.length).toBe(1);
+});
 
-test('should not warning checking props', () => {
-  const propsDefault = { secretWord: 'te' }
-  checkProps(Input, propsDefault)
-})
+test("should not warning checking props", () => {
+  const propsDefault = { secretWord: "te" };
+  checkProps(Input, propsDefault);
+});
 
-describe('state controlled input field', () => {
-  test('should state updates with value of input box upon change', () => {
-    const mockSetCurrentGuess = jest.fn()
-    React.useState = jest.fn(() => ['', mockSetCurrentGuess])
+describe("state controlled input field", () => {
+  let mockSetCurrentGuess = jest.fn();
+  let wrapper;
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+    wrapper = setup();
+  });
 
-    const wrapper = setup()
-    const inputBox = findByTestAttr(wrapper, 'input-box')
+  test("should state updates with value of input box upon change", () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
 
-    const mockEvent = { target: { value: 'train' }}
-    inputBox.simulate('change', mockEvent)
+    const mockEvent = { target: { value: "train" } };
+    inputBox.simulate("change", mockEvent);
 
-    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train')
-  })
-  
-})
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
 
+  test("should clear state after submit form", () => {
+    const submitButton = findByTestAttr(wrapper, "submit-button");
 
+    submitButton.simulate("click", {preventDefault() {}});
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
+  });
+});
